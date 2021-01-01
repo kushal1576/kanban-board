f75fd0a676b7e38cd560fa0faf1b08403d20a6de
Kushal I <kushal1576@gmail.com>
2021-01-01 23:44:50 +0530

    Added AddNewItem and NewItemForm components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AddNewItem } from './AddNewItem'
 import { Card } from './Card'
 import { Column } from './Column'
 import { AppContainer } from './styles'
@@ -15,6 +16,7 @@ const App = () => {
       <Column text="Done">
         <Card text="Begin to use static typing" />
       </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
     </AppContainer>
   )
 }
