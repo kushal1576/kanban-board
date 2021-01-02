e992cb42b008f0e5bab0947afa565257d3c36006
Kushal I <kushal1576@gmail.com>
2021-01-02 16:09:52 +0530

    Implemented add task and list functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { Column } from './Column'
 import { AppContainer } from './styles'
 
 const App = () => {
-    const { state } = useAppState()
+    const { state, dispatch } = useAppState()
 
     return (
         <AppContainer>
             {state.lists.map((list, i) => (
-                <Column text={list.text} key={list.id} index={i} />
+                <Column id={list.id} text={list.text} key={list.id} index={i} />
             ))}
             <AddNewItem
                 toggleButtonText="+ Add another list"
-                onAdd={console.log}
+                onAdd={text => dispatch({ type: "ADD_LIST", payload: text })}
             />
         </AppContainer>
     )
--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext } from "react"
+import { nanoid } from "nanoid"
+import React, { createContext, useContext, useReducer } from "react"
+import { findItemIndexById, overrideItemAtIndex } from "./utils/arrayUtils"
 
 interface Task {
     id: string
@@ -37,13 +39,67 @@ const appData: AppState = {
 
 interface AppStateContextProps {
     state: AppState
+    dispatch: React.Dispatch<Action>
+}
+
+type Action =
+    | {
+        type: "ADD_LIST"
+        payload: string
+    }
+    | {
+        type: "ADD_TASK"
+        payload: { text: string, listId: string }
+    }
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+    switch (action.type) {
+        case "ADD_LIST": {
+            return {
+                ...state,
+                lists: [
+                    ...state.lists,
+                    { id: nanoid(), text: action.payload, tasks: [] }
+                ]
+            }
+        }
+        case "ADD_TASK": {
+            const targetListIndex = findItemIndexById(
+                state.lists,
+                action.payload.listId
+            )
+
+            const targetList = state.lists[targetListIndex]
+
+            const updatedTargetList = {
+                ...targetList,
+                tasks: [
+                    ...targetList.tasks,
+                    { id: nanoid(), text: action.payload.text }
+                ]
+            }
+            return {
+                ...state,
+                lists: overrideItemAtIndex(
+                    state.lists,
+                    updatedTargetList,
+                    targetListIndex
+                )
+            }
+        }
+        default: {
+            return state
+        }
+    }
 }
 
 const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
 
 export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+    const [state, dispatch] = useReducer(appStateReducer, appData)
+
     return (
-        <AppStateContext.Provider value={{ state: appData }}>
+        <AppStateContext.Provider value={{ state, dispatch }}>
             {children}
         </AppStateContext.Provider>
     )
