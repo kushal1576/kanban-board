49b6900b0e4b159e17b09e87338868c3725c190d
Kushal I <kushal1576@gmail.com>
2021-01-02 13:11:20 +0530

    Enabled focus in form and added AppStateContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import { AddNewItem } from './AddNewItem'
-import { Card } from './Card'
+import { useAppState } from './AppStateContext'
 import { Column } from './Column'
 import { AppContainer } from './styles'
 
 const App = () => {
-  return (
-    <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
-    </AppContainer>
-  )
+    const { state } = useAppState()
+
+    return (
+        <AppContainer>
+            {state.lists.map((list, i) => (
+                <Column text={list.text} key={list.id} index={i} />
+            ))}
+            <AddNewItem
+                toggleButtonText="+ Add another list"
+                onAdd={console.log}
+            />
+        </AppContainer>
+    )
 }
 
 export default App
--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles"
+import { useFocus } from "./utils/useFocus"
 
 interface NewItemFormProps {
     onAdd(text: string): void
@@ -7,12 +8,22 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState("")
+    const inputRef = useFocus()
+    const handleAddText = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (event.key === "Enter") {
+            onAdd(text)
+        }
+    }
 
     return (
         <NewItemFormContainer>
             <NewItemInput
+                ref={inputRef}
                 value={text}
                 onChange={e => setText(e.target.value)}
+                onKeyPress={handleAddText}
             />
             <NewItemButton onClick={() => onAdd(text)}>
                 Create
